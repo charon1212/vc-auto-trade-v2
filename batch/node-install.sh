648863b6f4ec15edfcaff7a4a032db7521920e33
charon1212 <charons2turquoise@gmail.com>
2021-12-11 20:52:42 +0900

    add node-install batch

---
+++ batch/node-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+###########################################################################
+# CAUTION
+# Check Nvm Download Url.
+# see: https://docs.aws.amazon.com/ja_jp/sdk-for-javascript/v2/developer-guide/setting-up-node-on-ec2-instance.html
+###########################################################################
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
