13b50ec6b21c7af9c10f4f8952644df8d30f3e1c
charon1212 <charons2turquoise@gmail.com>
2021-12-15 19:12:01 +0900

    debug commit

--- src/debug.ts
+++ src/debug.ts
@@ -12,29 +12,8 @@ const debug = async () => {
   loadDotEnv();
   await resetConnection();
 
-  // const manager = new PriceManager('btc_jpy');
-  // manager.start();
-
-  // const sampledata = [
-  //   { timestamp: 100, price: 1 },
-  //   { timestamp: 200, price: 2 },
-  //   { timestamp: 300, price: 3 },
-  //   { timestamp: 400, price: 4 },
-  //   { timestamp: 500, price: 5 },
-  //   { timestamp: 600, price: 6 },
-  //   { timestamp: 700, price: 7 },
-  //   { timestamp: 800, price: 8 },
-  //   { timestamp: 900, price: 9 },
-  //   { timestamp: 1000, price: 10 },
-  // ];
-  // getConnection().manager.save(sampledata.map((d) => new PriceHistory(d)));
-  let cnt = 1;
-  cron.schedule('0 * * * * *', () => { // 毎分
-    const data = [1, 2, 3, 4, 5].map((i) => ({ timestamp: cnt * 10 + i, price: i })).map((v) => new PriceHistory(v));
-    logger.debug({ cnt, data, });
-    getConnection().manager.save(data);
-    cnt++;
-  });
+  const manager = new PriceManager('btc_jpy');
+  manager.start();
 
 };
 
