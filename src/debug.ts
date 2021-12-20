384c35ee9093a046154372af97833cbbc5d46ae4
charon1212 <charons2turquoise@gmail.com>
2021-12-21 08:24:11 +0900

    debug commit

--- src/debug.ts
+++ src/debug.ts
@@ -11,6 +11,7 @@ const debug = async () => {
   await resetConnection();
 
   const priceManager = new PriceManager('btc_jpy');
+  priceManager.start();
 
   const container = new StrategyBoxContainer();
   container.boxList.push(new StrategyBox1('btc_jpy', priceManager));
