b4971c3d7aa949f3b0fb5e02bc9cbff795efc6fd
charon1212 <charons2turquoise@gmail.com>
2021-12-25 00:22:18 +0900

    add price-history endpoint(test)

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { PriceHistory } from '../typeorm/entity/PriceHistory';
+import { getConnection, resetConnection } from '../typeorm/typeorm';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,4 +11,12 @@ app.listen(3000, () => {
 
 app.get('/test', (request, response) => {
   response.send(JSON.stringify({ message: 'hello expressjs!' }));
+});
+
+app.get('/vcat2/v1/pair/:pair/price-history', async (request, response) => {
+  await resetConnection();
+  const pair = request.params.pair;
+  const conn = getConnection();
+  const result = await conn.manager.find(PriceHistory);
+  response.send(JSON.stringify({ pair, result }));
 });
