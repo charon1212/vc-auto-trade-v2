213bb6c767cfc4eab0b012145ebf2bfdcfd02bf1
charon1212 <charons2turquoise@gmail.com>
2021-12-24 23:14:28 +0900

    update

--- projects/aws-api-2/lib/lambda/hello.ts
+++ projects/aws-api-2/lib/lambda/hello.ts
@@ -3,7 +3,13 @@ import { Handler } from 'aws-lambda'
 // Lambda エントリーポイント
 export const handler: Handler = async () => {
   console.log('Hello Lambda!')
-  return {
+  const body = {
     message: 'hello aws-cdk!',
+  };
+  return {
+    isBase64Encoded: false,
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify(body),
   }
 }
