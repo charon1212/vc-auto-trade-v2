b6ae652ba537d9c8bfdeb22d270d71c54bb66e6e
charon1212 <charons2turquoise@gmail.com>
2021-12-25 12:09:32 +0900

    update PriceManager

--- src/domain/price/PriceManager.ts
+++ src/domain/price/PriceManager.ts
@@ -5,7 +5,7 @@ import { getConnection } from "../../typeorm/typeorm";
 import { logger } from "../../common/log/logger";
 import { apiTicker } from "../../interfaces/coincheck/apiTicker";
 
-export type PriceHistoryData = { timestamp: number, price: number };
+export type PriceHistoryData = { timestamp: number, price: number, lostData: boolean, };
 /**
  * 市場の取引状況を監視し、過去の価格履歴や最新価格を提供する。
  */
@@ -19,26 +19,38 @@ export class PriceManager {
   constructor(protected pair: Pair) { }
 
   /**
+   * 過去1時間のShortHistoryの中に、連続して2点の欠損データがある場合、無効データとしてfalseを返却する。
+   */
+  checkShortHistoryIsValid() {
+    let lossCount = 0;
+    for (let i = 0; i < Math.min(this.shortHistory.length, 360); i++) {
+      if (this.shortHistory[i].lostData) lossCount++;
+      else lossCount = 0;
+      if (lossCount >= 2) return false;
+    }
+    return true;
+  }
+
+  /**
    * 市場の取引状況監視を始める。
    */
   start() {
     // 10秒ごとに最新価格をshortHistoryにため込む
     cron.schedule('*/10 * * * * *', async () => {
       const ticker = await apiTicker(this.pair);
+      const timespan = 10000;
+      const timestamp = Math.round(Date.now() / timespan) * timespan;
       if (ticker) {
-        const timespan = 10000;
-        const timestamp = Math.round(Date.now() / timespan) * timespan;
-        this.shortHistory.push({ timestamp, price: ticker.last });
+        const price = ticker.last;
+        this.shortHistory.push({ timestamp, price, lostData: false });
+        await getConnection().manager.save(new PriceHistory({ timestamp: timestamp.toString(), price }));
+      } else if (this.shortHistory.length > 0) { // 過去のデータがある場合、欠損データとして1個前のデータで登録
+        this.shortHistory.push({ timestamp, price: this.shortHistory[this.shortHistory.length - 1].price, lostData: true });
       }
     });
-    // 1時間ごとに、1時間以上前のshortHistoryをDBに保存してメモリから削除する。
+    // 1時間ごとに、1時間以上前のshortHistoryを削除する。
     cron.schedule('0 0 * * * *', async () => {
       const before1h = Date.now() - 60 * 60 * 1000;
-      const saveData = this.shortHistory.filter((data: PriceHistoryData) => data.timestamp < before1h);
-      if (saveData.length > 0) {
-        const saveDataEntity = saveData.map(({ timestamp, price }) => new PriceHistory({ timestamp: timestamp.toString(), price, pair: this.pair }));
-        await getConnection().manager.save(saveDataEntity);
-      }
       this.shortHistory = this.shortHistory.filter((data: PriceHistoryData) => data.timestamp >= before1h);
     });
   }
