663e5508941e209be9526ce8d8ceff9acc55ce24
charon1212 <charons2turquoise@gmail.com>
2021-12-26 15:22:08 +0900

    update processenv

--- src/common/dotenv/processEnv.ts
+++ src/common/dotenv/processEnv.ts
@@ -8,6 +8,11 @@ export let processEnv = {
   TYPEORM_DATABASE: '',
   LOG_LEVEL: '',
   LOG_PATH: '',
+  COINCHECK_API_KEY: '',
+  COINCHECK_SECRET_KEY: '',
+  SLACK_BOT_AUTH_TOKEN: '',
+  SLACK_CHANNEL_INFO: '',
+  SLACK_CHANNEL_ERROR: '',
 };
 /**
  * 環境変数を読み込む。
@@ -22,5 +27,10 @@ export const loadDotEnv = () => {
     TYPEORM_DATABASE: process.env['TYPEORM_DATABASE'] || '',
     LOG_LEVEL: process.env['LOG_LEVEL'] || '',
     LOG_PATH: process.env['LOG_PATH'] || '',
+    COINCHECK_API_KEY: process.env['COINCHECK_API_KEY'] || '',
+    COINCHECK_SECRET_KEY: process.env['COINCHECK_SECRET_KEY'] || '',
+    SLACK_BOT_AUTH_TOKEN: process.env['SLACK_BOT_AUTH_TOKEN'] || '',
+    SLACK_CHANNEL_INFO: process.env['SLACK_CHANNEL_INFO'] || '',
+    SLACK_CHANNEL_ERROR: process.env['SLACK_CHANNEL_ERROR'] || '',
   };
 };
