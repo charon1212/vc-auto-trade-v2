ff0cff9b7f5d0150462b9e6f9383e4537b0d1e0b
charon1212 <charons2turquoise@gmail.com>
2021-12-26 18:37:15 +0900

    add getPriceHistory

---
+++ projects/vcat2-analyze-tool/src/interfaces/getPriceHistory.ts
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+
+export type Params = {
+  pair: 'btc_jpy',
+  minTimestamp?: number,
+  maxTimestamp?: number,
+};
+export const getPriceHistory = async (params: Params) => {
+
+  const { pair, minTimestamp, maxTimestamp } = params;
+  const queryParams = [];
+  if (minTimestamp !== undefined) queryParams.push(`min-timestamp=${minTimestamp}`);
+  if (maxTimestamp !== undefined) queryParams.push(`max-timestamp=${maxTimestamp}`);
+  const query = queryParams.join('&');
+
+  let url = (process.env['REACT_APP_VCAT2_URL'] || '') + `/pair/${pair}/price-history`;
+  if (query !== '') url += `?${query}`;
+  const data = await fetch(url);
+  console.log(url, data);
+  const json = await data.json();
+  console.log(json);
+  return json;
+
+};
