d33884d375824be06b635afadf5b06a88a1fe3c9
charon1212 <charons2turquoise@gmail.com>
2022-01-01 17:47:01 +0900

    add

--- projects/vcat2-analyze-tool/src/components/Main.tsx
+++ projects/vcat2-analyze-tool/src/components/Main.tsx
@@ -1,8 +1,8 @@
 import Graph from './Graph';
-import MyTabContainer from './MyTabContainer';
-import { useDateSelector } from './useDateSelector';
-import { usePriceHistory } from './usePriceHistory';
-import { useTradeResult } from './useTradeResult';
+import MyTabContainer from './util/MyTabContainer';
+import { useDateSelector } from './hooks/useDateSelector';
+import { usePriceHistory } from './hooks/usePriceHistory';
+import { useTradeResult } from './hooks/useTradeResult';
 
 const Main = () => {
   const { date, setDate, DateSelector } = useDateSelector();
@@ -21,6 +21,12 @@ const Main = () => {
                 <Graph priceHistory={priceHistory.filter((v, i) => i % 6 === 0)} tradeResult={tradeResult} height={600} />
               </div>
             ),
+          },
+          {
+            title: '結果まとめ',
+            contents: <div style={{ margin: '20px' }}>
+
+            </div>,
           },
         ]}
       />
