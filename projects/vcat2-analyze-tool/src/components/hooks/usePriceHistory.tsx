4d5d2ae9db262b34562c9cd06a40e79b5738225d
charon1212 <charons2turquoise@gmail.com>
2022-01-01 17:54:06 +0900

    add type definition

--- projects/vcat2-analyze-tool/src/components/hooks/usePriceHistory.tsx
+++ projects/vcat2-analyze-tool/src/components/hooks/usePriceHistory.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import startOfDay from 'date-fns/startOfDay';
 import { getPriceHistory } from '../../interfaces/getPriceHistory';
 
+export type APriceHistory = { timestamp: number; price: number };
+
 export const usePriceHistory = (params: { pair: 'btc_jpy'; date?: Date }) => {
   const { pair, date } = params;
-  const [priceHistory, setPriceHistory] = useState<{ timestamp: number; price: number }[]>([]);
+  const [priceHistory, setPriceHistory] = useState<APriceHistory[]>([]);
   useEffect(() => {
     if (date) {
       const startTimestamp = startOfDay(date).getTime();
--- projects/vcat2-analyze-tool/src/components/hooks/useTradeResult.tsx
+++ projects/vcat2-analyze-tool/src/components/hooks/useTradeResult.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import startOfDay from 'date-fns/startOfDay';
 import { getTradeResult } from '../../interfaces/getTradeResult';
 
+export type ATradeResult = { timestamp: number; price: number; side: 'buy' | 'sell' };
+
 export const useTradeResult = (params: { date?: Date }) => {
   const { date } = params;
-  const [tradeResult, setTradeResult] = useState<{ timestamp: number; price: number; side: 'buy' | 'sell' }[]>([]);
+  const [tradeResult, setTradeResult] = useState<ATradeResult[]>([]);
   useEffect(() => {
     if (date) {
       const startTimestamp = startOfDay(date).getTime();
