82ab1d0fbbd27a0031a58b7968cbda77ee704bde
charon1212 <charons2turquoise@gmail.com>
2022-01-05 22:30:32 +0900

    delete auth

--- projects/vcat2-analyze-tool/src/App.tsx
+++ projects/vcat2-analyze-tool/src/App.tsx
@@ -1,12 +1,9 @@
-import Auth from './components/Auth';
 import Main from './components/Main';
 
 const App = () => {
   return (
     <>
-      <Auth>
-        <Main />
-      </Auth>
+      <Main />
     </>
   );
 };
