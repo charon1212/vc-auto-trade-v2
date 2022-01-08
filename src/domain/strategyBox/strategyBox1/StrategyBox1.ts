d06952785e4908f3fa536fc2abe790fa7fb06a44
charon1212 <charons2turquoise@gmail.com>
2022-01-08 15:52:58 +0900

    add param

--- src/domain/strategyBox/strategyBox1/StrategyBox1.ts
+++ src/domain/strategyBox/strategyBox1/StrategyBox1.ts
@@ -4,22 +4,37 @@ import { sendMyTradeDummy } from "../../trade/MyTradeDummy";
 import { StrategyBoxBase } from "../StrategyBoxBase";
 import { judgeStrategyBox1 } from "./judge";
 
+/** コンテキスト定義 */
 export type ContextStrategyBox1 = { position: 'rc' | 'vc', };
 export const initContextStrategyBox1: ContextStrategyBox1 = { position: 'rc' };
+/** パラメータ定義 */
+export type ParamStrategyBox1 = {
+  macdShort: number,
+  macdLong: number,
+  threshold: number,
+  reverse: boolean,
+};
+export const initParamStrategyBox1: ParamStrategyBox1 = {
+  macdShort: 5,
+  macdLong: 30,
+  threshold: 0.0003,
+  reverse: true,
+};
 
+/**
+ * MACDが一定の基準値を上回る/下回る時にトレードする。
+ */
 export class StrategyBox1 extends StrategyBoxBase<ContextStrategyBox1> {
+
+  /** コンテキスト */
   protected isContextType = (arg: any): arg is ContextStrategyBox1 => {
     if (typeof arg !== 'object') return false;
     if (arg?.position !== 'rc' && arg?.position !== 'vc') return false;
     return true;
   };
 
-  static getCreator(creatorParams: { id: string }, postConstructor?: (instance: StrategyBox1) => StrategyBox1) {
-    return (params: StrategyBoxCreatorParams) => {
-      const newStrategyBox1 = new StrategyBox1(creatorParams.id, params.pair, params.priceManager, initContextStrategyBox1);
-      return postConstructor ? postConstructor(newStrategyBox1) : newStrategyBox1;
-    };
-  };
+  /** パラメータ */
+  public param: ParamStrategyBox1 = initParamStrategyBox1;
 
   protected tick(next: () => unknown): void {
     if (this.priceManager.shortHistory.length < 360) { // 1時間分のデータが溜まってないなら、いったん保留にする。
@@ -28,7 +43,7 @@ export class StrategyBox1 extends StrategyBoxBase<ContextStrategyBox1> {
       return;
     }
     const { position } = this.context;
-    const judge = judgeStrategyBox1(this.priceManager.shortHistory.map((v) => v.price));
+    const judge = judgeStrategyBox1(this.priceManager.shortHistory.map((v) => v.price), this.param);
     let side: '' | 'buy' | 'sell' = '';
     if (position === 'rc' && judge === 'vc') side = 'buy'
     if (position === 'vc' && judge === 'rc') side = 'sell'
@@ -45,5 +60,13 @@ export class StrategyBox1 extends StrategyBoxBase<ContextStrategyBox1> {
       next();
     }
   }
+
+  /** Creator */
+  static getCreator(creatorParams: { id: string }, postConstructor?: (instance: StrategyBox1) => StrategyBox1) {
+    return (params: StrategyBoxCreatorParams) => {
+      const newStrategyBox1 = new StrategyBox1(creatorParams.id, params.pair, params.priceManager, initContextStrategyBox1);
+      return postConstructor ? postConstructor(newStrategyBox1) : newStrategyBox1;
+    };
+  };
 
 };
--- src/domain/strategyBox/strategyBox1/judge.ts
+++ src/domain/strategyBox/strategyBox1/judge.ts
@@ -1,14 +1,17 @@
 import { calcMacd } from "../../../tradingLogic/macd";
+import { ParamStrategyBox1 } from "./StrategyBox1";
 
 export type Judge = 'keep' | 'rc' | 'vc';
 
-export const judgeStrategyBox1 = (priceList: number[]): Judge => {
+export const judgeStrategyBox1 = (priceList: number[], param: ParamStrategyBox1): Judge => {
+
+  const { macdShort, macdLong, threshold, reverse } = param;
 
   if (priceList.length === 0) return 'keep';
-  const { macd, macdSignal } = calcMacd(priceList, 5, 30, 10);
+  const { macd } = calcMacd(priceList, macdShort, macdLong, 10);
   const macdRate = macd[macd.length - 1] / priceList[priceList.length - 1];
-  if (macdRate > 0.0003) return 'vc';
-  if (macdRate < -0.0003) return 'rc';
+  if (macdRate > threshold) return reverse ? 'vc' : 'rc';
+  if (macdRate < -threshold) return reverse ? 'rc' : 'vc';
   return 'keep';
 
 };
