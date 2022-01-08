179f247ccb8019b91c95dca14e3d4bc4d4ee36b0
charon1212 <charons2turquoise@gmail.com>
2022-01-08 14:54:33 +0900

    debug commit

--- src/debug.ts
+++ src/debug.ts
@@ -1,14 +1,8 @@
-import { PriceManager } from "./domain/price/PriceManager";
-import { StrategyBox1 } from "./domain/strategyBox/strategyBox1/StrategyBox1";
 import { startup } from "./startup";
 
 const debug = async () => {
 
   await startup();
-
-  const pm = new PriceManager('btc_jpy');
-  const sb1_test = StrategyBox1.creator({ id: 'test-1' })({ pair: 'btc_jpy', priceManager: pm });
-  sb1_test.loadContextFromFile();
 
 };
 
