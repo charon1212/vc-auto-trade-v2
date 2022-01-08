05c23ef2933ddf0b3f0d54467168ccf35fd189b1
charon1212 <charons2turquoise@gmail.com>
2022-01-08 17:14:17 +0900

    add Strategy Box

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,14 @@ const index = async () => {
 
   const container = new StrategyBoxContainer();
   container.addStrategyBox('btc_jpy', StrategyBox1.getCreator({ id: 'sb-1' }));
+  container.addStrategyBox('btc_jpy', StrategyBox1.getCreator({ id: 'sb-1-reverse' }, (instance) => {
+    instance.param.reverse = true;
+    return instance;
+  }));
+  container.addStrategyBox('btc_jpy', StrategyBox1.getCreator({ id: 'sb-1-threshold2' }, (instance) => {
+    instance.param.threshold = 0.0002;
+    return instance;
+  }));
   container.start();
 
 };
