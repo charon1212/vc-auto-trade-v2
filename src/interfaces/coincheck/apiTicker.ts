65175454b26c03f75ed7cb368bc8df1850c54b3b
charon1212 <charons2turquoise@gmail.com>
2022-05-01 15:15:05 +0900

    add comment

--- src/interfaces/coincheck/apiTicker.ts
+++ src/interfaces/coincheck/apiTicker.ts
@@ -4,19 +4,19 @@ import { Pair } from "../../type/coincheck";
 import { sendApiRequest } from "./sendApiRequest";
 
 export type ApiResultTicker = {
-  last: number,
-  bid: number,
-  ask: number,
-  high: number,
-  low: number,
-  volume: number,
-  timestamp: number
+  last: number, // 最後の取引の価格
+  bid: number, // 現在の買い注文の最高価格
+  ask: number, // 現在の売り注文の最安価格
+  high: number, // 24時間での最高取引価格
+  low: number, // 24時間での最安取引価格
+  volume: number, // 24時間での取引量
+  timestamp: number // 現在の時刻
 };
 /**
  * /api/tickerにリクエストを送信し、ティッカー情報を取得する。
  *
- * @param pair 通貨ペア
- * @returns 指定した通貨ペアのティッカー情報（最新の取引価格や、24hの最安/最高値）。
+ * @param pair 取引ペア
+ * @returns 指定した取引ペアのティッカー情報（最新の取引価格や、24hの最安/最高値）。
  */
 export const apiTicker = async (pair: Pair) => {
   const requestResult = await sendApiRequest({
--- src/interfaces/coincheck/postApiExchangeOrder.ts
+++ src/interfaces/coincheck/postApiExchangeOrder.ts
@@ -4,23 +4,29 @@ import { Pair } from "../../type/coincheck";
 import { sendApiRequest } from "./sendApiRequest";
 
 export type Param = {
-  pair: Pair,
-  side: 'buy' | 'sell',
-  orderType: 'limit' | 'market',
-  rate?: number,
-  amount: number,
+  pair: Pair, // 取引ペア
+  side: 'buy' | 'sell', // 売り/買い
+  orderType: 'limit' | 'market', // limit: 指値、market: 成行
+  rate?: number, // 指値の取引レート。成行の場合は任意。
+  amount: number, // 取引量(単位は仮想通貨)
 };
 export type Result = {
-  success: boolean,
-  id: number,
-  rate: string,
-  amount: string,
-  order_type: 'buy' | 'sell' | 'market_buy' | 'market_sell',
-  pair: Pair,
+  success: boolean, // 正常終了なので、true。
+  id: number, // 新規注文ID
+  rate: string, // 注文のレート(1単位仮想通貨あたり何円)
+  amount: string, // 注文量(単位は仮想通貨)
+  order_type: 'buy' | 'sell' | 'market_buy' | 'market_sell', // 注文のタイプ。売り買いと、成行/指値
+  pair: Pair, // 取引ペア
   stop_loss_rate: unknown, // 逆指値レートとして、何が返却されるかよくわからん。
   market_buy_amount: unknown, // 良くわからん。日本円でどの程度の量を買うか指定できる？
 };
 
+/**
+ * 新規注文
+ *
+ * @param param パラメータ。詳細は型定義を参照。
+ * @returns リクエスト結果のObject。エラーの場合はundefined。
+ */
 export const postApiExchangeOrder = async (param: Param) => {
   const { pair, side, orderType, rate, amount } = param;
   const order_type = (orderType === 'market' ? 'market_' : '') + (side === 'buy' ? 'buy' : 'sell');
