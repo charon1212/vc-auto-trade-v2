372c1f43e4012360b1265812bf53cf161f2db8a2
charon1212 <charons2turquoise@gmail.com>
2022-05-04 08:33:54 +0900

    add param[amountMarketBuy] add validation check

--- src/interfaces/coincheck/postApiExchangeOrder.ts
+++ src/interfaces/coincheck/postApiExchangeOrder.ts
@@ -8,7 +8,8 @@ export type Param = {
   side: 'buy' | 'sell', // 売り/買い
   orderType: 'limit' | 'market', // limit: 指値、market: 成行
   rate?: number, // 指値の取引レート。成行の場合は任意。
-  amount: number, // 取引量(単位は仮想通貨)
+  amount?: number, // 取引量(単位は仮想通貨)
+  amountMarketBuy?: number, // 取引量(単位は日本円)
 };
 export type Result = {
   success: boolean, // 正常終了なので、true。
@@ -28,13 +29,22 @@ export type Result = {
  * @returns リクエスト結果のObject。エラーの場合はundefined。
  */
 export const postApiExchangeOrder = async (param: Param) => {
-  const { pair, side, orderType, rate, amount } = param;
+  // 引数チェック
+  const { hasValidationError, message } = validationPostApiExchangeOrder(param);
+  if (hasValidationError) {
+    logger.error(`[APIエラー:postApiExchangeOrder][バリデーションエラー:${message}]`);
+    handleError({ __filename, method: 'postApiExchangeOrder', });
+    return undefined;
+  }
+
+  const { pair, side, orderType, rate, amount, amountMarketBuy } = param;
   const order_type = (orderType === 'market' ? 'market_' : '') + (side === 'buy' ? 'buy' : 'sell');
+  const market_buy_amount = amountMarketBuy;
   const requestResult = await sendApiRequest({
     uri: '/api/exchange/orders',
     method: 'POST',
     isPrivate: true,
-    body: JSON.stringify({ pair, order_type, rate, amount, }),
+    body: JSON.stringify({ pair, order_type, rate, amount, market_buy_amount }),
   });
   if (requestResult.success) {
     return requestResult.responseBody as Result;
@@ -43,4 +53,17 @@ export const postApiExchangeOrder = async (param: Param) => {
     handleError({ __filename, method: 'postApiExchangeOrder', });
     return undefined;
   }
+};
+
+const validationPostApiExchangeOrder = (param: Param) => {
+  const { side, orderType, rate, amount, amountMarketBuy } = param;
+  if (orderType === 'limit' && !rate) {
+    return { hasValidationError: true, message: '指値注文では、rateが必須です。', };
+  }
+  if (orderType === 'market' && side === 'buy' && !amountMarketBuy) {
+    return { hasValidationError: true, message: '成行買い注文では、amountMarketBuyが必須です。', };
+  } else if ((orderType === 'limit' || side === 'sell') && !amount) {
+    return { hasValidationError: true, message: '指値注文、または売り注文では、amountが必須です。', };
+  }
+  return { hasValidationError: false, message: '', };
 };
