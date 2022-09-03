fc676a54e121c2dca81e67031ff1dac1deae4f40
charon1212 <charons2turquoise@gmail.com>
2022-09-03 19:57:37 +0900

    add cache

---
+++ src/domain2/Trade/TradeCache.ts
@@ -0,0 +1,38 @@
+import { findTrade } from "../../lib/typeorm/repository/Trade/findTrade";
+import { insertTrade } from "../../lib/typeorm/repository/Trade/insertTrade";
+import { updateTrade } from "../../lib/typeorm/repository/Trade/updateTrade";
+import { Trade, TradeStatus } from "./Trade";
+
+export class TradeCache {
+  private cacheAll: Trade[] = [];
+  private cache: { [status in TradeStatus]: Trade[] } = { requesting: [], requested: [], executed: [], cancel: [] };
+
+  constructor() { };
+  async setupCache() {
+    const allTrade = await findTrade();
+    this.cacheAll = allTrade;
+    this.cache.requesting = allTrade.filter(({ status }) => status === 'requesting');
+    this.cache.requested = allTrade.filter(({ status }) => status === 'requested');
+    this.cache.executed = allTrade.filter(({ status }) => status === 'executed');
+    this.cache.cancel = allTrade.filter(({ status }) => status === 'cancel');
+  };
+  getCache(status?: TradeStatus): Trade[] {
+    return status ? this.cache[status] : this.cacheAll;
+  }
+  async add(trade: Trade) {
+    this.cacheAll.push(trade);
+    this.cache[trade.status].push(trade);
+    await insertTrade(trade);
+  };
+  async changeStatus(tradeUid: string, status: TradeStatus) {
+    const trade = this.cacheAll.find(({ uid }) => uid === tradeUid);
+    if (!trade) {
+      throw new Error(''); // TODO: エラー処理
+    };
+    const beforeStatus = trade.status;
+    trade.status = status;
+    this.cache[beforeStatus].filter(({ uid }) => uid !== tradeUid);
+    this.cache[status].push(trade);
+    await updateTrade(trade);
+  };
+};
--- src/domain2/Trade/TradeManager.ts
+++ src/domain2/Trade/TradeManager.ts
@@ -6,36 +6,29 @@ import { insertTrade } from "../../lib/typeorm/repository/Trade/insertTrade";
 import { updateTrade } from "../../lib/typeorm/repository/Trade/updateTrade";
 import { Execution } from "../Execution/Execution";
 import { Trade, TradeStatus } from "./Trade";
+import { TradeCache } from "./TradeCache";
 
 class TradeManager {
-  private cacheAll: Trade[] = [];
-  private cache: { [status in TradeStatus]: Trade[] } = { requesting: [], requested: [], executed: [], cancel: [] };
+  private tradeCache = new TradeCache();
 
   constructor() { };
 
   async setupCache() {
-    const allTrade = await findTrade();
-    this.cacheAll = allTrade;
-    this.cache.requesting = allTrade.filter(({ status }) => status === 'requesting');
-    this.cache.requested = allTrade.filter(({ status }) => status === 'requested');
-    this.cache.executed = allTrade.filter(({ status }) => status === 'executed');
-    this.cache.cancel = allTrade.filter(({ status }) => status === 'cancel');
+    await this.tradeCache.setupCache();
   };
 
   async order(trade: Trade) {
-    this.cacheAll.push(trade);
-    this.cache.requesting.push(trade);
-    await insertTrade(trade);
+    await this.tradeCache.add(trade);
     const apiId = await postOrder(trade);
     if (apiId === undefined) {
-      return; // TODO: エラー処理
+      throw new Error(''); // TODO: エラー処理
     }
     trade.apiId = apiId;
-    await this.changeTradeStatus(trade, 'requested');
+    await this.tradeCache.changeStatus(trade.uid, 'requested');
   };
 
   async setExecution(execution: Execution) {
-    const trade = this.cacheAll.find(({ uid }) => uid === execution.tradeUid);
+    const trade = this.tradeCache.getCache().find(({ uid }) => uid === execution.tradeUid);
     if (!trade) return false;
     trade.executions.push(execution);
     // 約定量が注文量の99%以上の場合、未決済の注文一覧を確認し、そこにIDがなければexecutedとする。
@@ -44,31 +37,22 @@ class TradeManager {
       const openOrderIdList = await fetchOpenOrderIdList();
       if (!openOrderIdList) return false;
       if (!openOrderIdList.includes(trade.apiId)) {
-        await this.changeTradeStatus(trade, 'executed');
+        await this.tradeCache.changeStatus(trade.uid, 'executed');
       }
     }
     return true;
   }
 
   getTradeIdMap() {
-    return this.cacheAll.map(({ uid, apiId }) => ({ uid, apiId }));
+    return this.tradeCache.getCache().map(({ uid, apiId }) => ({ uid, apiId }));
   }
 
   getTradeListByStrategyBoxId(strategyBoxId: string) {
-    return this.cacheAll.filter((trade) => trade.strategyBoxId === strategyBoxId);
+    return this.tradeCache.getCache().filter((trade) => trade.strategyBoxId === strategyBoxId);
   };
 
-  private async changeTradeStatus(trade: Trade, status: TradeStatus) {
-    const before = trade.status;
-    trade.status = status;
-    trade.lastUpdateStatusMs = Date.now();
-    this.cache[before] = this.cache[before].filter((t) => t !== trade);
-    this.cache[status].push(trade);
-    await updateTrade(trade);
-  }
-
   getCache(status?: TradeStatus): DR<Trade[]> {
-    return status ? this.cache[status] : this.cacheAll;
+    return this.tradeCache.getCache(status);
   }
 
 };
