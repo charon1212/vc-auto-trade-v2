6590adb09341e5d2a785991039445a2d637386c7
charon1212 <charons2turquoise@gmail.com>
2022-09-08 23:05:22 +0900

    add spy common modules.

---
+++ test/util/spyCommon.test.ts
@@ -0,0 +1,12 @@
+import { handleError } from "../../src/common/error/handleError";
+import { spyCommon } from "./spyCommon";
+
+describe('spyCommon', () => {
+  it('test1', () => {
+    const { spyHandleError } = spyCommon();
+    handleError({ __filename: 'test1-1', method: 'test1-2' });
+    expect(spyHandleError).toBeCalled();
+    expect(spyHandleError.mock.calls[0][0].__filename).toBe('test1-1');
+    expect(spyHandleError.mock.calls[0][0].method).toBe('test1-2');
+  });
+});
---
+++ test/util/spyCommon.ts
@@ -0,0 +1,23 @@
+import { processEnv } from '../../src/common/dotenv/processEnv';
+import * as handleError from '../../src/common/error/handleError';
+import { logger } from '../../src/common/log/logger';
+
+export type SpyCommonParams = {
+  mockProcessEnv?: Partial<typeof processEnv>,
+};
+export const spyCommon = (param?: SpyCommonParams) => {
+  const mockProcessEnv = param?.mockProcessEnv;
+  // processEnv
+  for (let k in mockProcessEnv) (processEnv as any)[k] = (mockProcessEnv as any)[k];
+  // handleError
+  const spyHandleError = jest.spyOn(handleError, 'handleError').mockImplementation();
+  // logger
+  const error = jest.spyOn(logger, 'error').mockImplementation();
+  const warn = jest.spyOn(logger, 'warn').mockImplementation();
+  const info = jest.spyOn(logger, 'info').mockImplementation();
+  const debug = jest.spyOn(logger, 'debug').mockImplementation();
+  const trace = jest.spyOn(logger, 'trace').mockImplementation();
+
+  return { spyHandleError, spyLogger: { error, warn, info, debug, trace } };
+
+};
