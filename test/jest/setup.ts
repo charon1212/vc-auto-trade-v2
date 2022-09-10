9f1538734f8a4964aeab4af810338103afa1cbad
charon1212 <charons2turquoise@gmail.com>
2022-09-10 22:00:09 +0900

    remove test log.

--- test/jest/setup.ts
+++ test/jest/setup.ts
@@ -2,21 +2,17 @@ import 'dotenv/config';
 import { getQueryRunnerForTest, initializeTypeorm, releaseTypeorm } from "../../src/lib/typeorm/typeorm";
 
 beforeAll(async () => {
-  console.log('setup-before-all');
   await initializeTypeorm();
 });
 
 beforeEach(async () => {
-  console.log('setup-before-each');
   await getQueryRunnerForTest()?.startTransaction();
 });
 
 afterEach(async () => {
-  console.log('setup-after-each');
   await getQueryRunnerForTest()?.rollbackTransaction();
 });
 
 afterAll(async () => {
-  console.log('setup-after-all');
   await releaseTypeorm();
 });
