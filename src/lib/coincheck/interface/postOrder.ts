d2817e04aa9fe182cf01cd9230ff8919bc87798e
charon1212 <charons2turquoise@gmail.com>
2022-09-12 18:17:22 +0900

    refactoring

--- src/lib/coincheck/interface/postOrder.ts
+++ src/lib/coincheck/interface/postOrder.ts
@@ -6,21 +6,12 @@ import { CoincheckPostOrder } from "../apiTool/CoincheckPostOrder";
 
 export const postOrder = async (trade: DR<Trade>) => {
   const { pair, tradeParam } = trade;
-  if (tradeParam.type === 'limit') {
-    const { amount, rate, side, type } = tradeParam;
-    const result = await CoincheckPostOrder.request({ pair, amount, rate, side, type, });
-    return result?.id.toString();
-  } else if (tradeParam.type === 'market') {
-    const { amount, side, type } = tradeParam;
-    if (side === 'buy') {
-      const amountMarketBuy = calcAmountMarketBuy(pair, amount);
-      const result = await CoincheckPostOrder.request({ pair, side, type, amountMarketBuy });
-      return result?.id.toString();
-    } else if (side === 'sell') {
-      const result = await CoincheckPostOrder.request({ pair, side, type, amount });
-      return result?.id.toString();
-    }
-  }
+  const { side, type } = tradeParam;
+  const rate = tradeParam.type === 'limit' ? tradeParam.rate : undefined;
+  const amount = (tradeParam.type === 'market' && tradeParam.side == 'buy') ? undefined : tradeParam.amount;
+  const amountMarketBuy = (tradeParam.type === 'market' && tradeParam.side == 'buy') ? calcAmountMarketBuy(pair, tradeParam.amount) : undefined;
+  const result = await CoincheckPostOrder.request({ pair, side, type, rate, amount, amountMarketBuy });
+  return result?.id.toString();
 };
 
 const calcAmountMarketBuy = (pair: Pair, amount: number) => {
