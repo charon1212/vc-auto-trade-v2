743437a5505691ba210dfc17dd4293fcd80f1c6c
charon1212 <charons2turquoise@gmail.com>
2022-10-02 09:27:10 +0900

    add Delete Method to CROS.

--- src/lib/express/common/commonSetting.ts
+++ src/lib/express/common/commonSetting.ts
@@ -59,7 +59,7 @@ const useAuth = () => {
  */
 const useCors = () => app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, auth-token, access_token');
   if (req.method === 'OPTIONS') {
     res.send(200);
