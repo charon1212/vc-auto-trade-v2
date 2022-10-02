2b6a6b76c0a9f6ccec598769934c6f3a1117492a
charon1212 <charons2turquoise@gmail.com>
2022-10-02 11:49:02 +0900

    add debug code.

--- src/debug.ts
+++ src/debug.ts
@@ -1,8 +1,20 @@
 import { startup } from "./startup";
+import { timeMeasure } from "util-charon1212";
+import { findMarket } from "./lib/typeorm/repository/Market/findMarket";
 
 const debug = async () => {
 
   await startup('debug');
+
+  const result = await timeMeasure((complete) => {
+    findMarket({ pair: 'btc_jpy', limit: 10000, startTimestamp: 1664592503000 }).then((result) => {
+      console.log(result);
+      complete();
+    });
+  });
+
+  console.log(`****** TIME MEASURE ******`);
+  console.log(result);
 
 };
 
