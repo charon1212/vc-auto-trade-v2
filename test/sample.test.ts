d5be2c2233a718c2592522d5c7aa21cc21fbd54d
charon1212 <charons2turquoise@gmail.com>
2022-10-02 16:39:26 +0900

    remove old sample test.

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -9,12 +9,6 @@ describe('sample', () => {
     expect(2 + 3).toBe(5);
   });
 
-  it('sample test2', () => {
-    expect(marketCache.getPriceHistory('btc_jpy')).toBeUndefined();
-    marketCache.registerPair('btc_jpy');
-    expect(marketCache.getPriceHistory('btc_jpy')?.length).toBe(0);
-  });
-
   it('sample typeorm', async () => {
     await clearDbData(Market);
     const newMarket = new Market('btc_jpy', { price: 1, timestamp: Date.now() });
