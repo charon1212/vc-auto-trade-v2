d192237de84666ab10c3accf85461fad1d26a0d5
charon1212 <charons2turquoise@gmail.com>
2022-10-05 23:11:03 +0900

    refactoring Result Type.

--- src/common/error/Result.ts
+++ src/common/error/Result.ts
@@ -1,30 +1,39 @@
-export type Result<O, E> = ({ isOk: true, ok: O, er?: never, } | { isOk: false, ok?: never, er: E }) & {
-  unwrap: () => O;
-  match: <P, Q>(handler: { ok: (ok: O) => P, er: (er: E) => Q }) => P | Q;
-  handleOk: <P, E2 = never>(handler: (ok: O) => P, onEr?: (e: unknown) => E2) => Result<P, E | E2>;
-  handleEr: <P, E2 = never>(handler: (er: E) => P, onEr?: (e: unknown) => E2) => Result<O, P | E2>;
-};
+abstract class ResultBase<O, E>{
+  protected readonly __branc_result_base = '__branc_result_base';
+  abstract unwrap(): O;
+  abstract else<T>(def: T): O | T;
+  abstract handleOk<O2>(handler: (o: O) => O2): Result<O2, E>;
+  abstract handleEr<E2>(handler: (e: E) => E2): Result<O, E2>;
+  abstract match<O2, E2>(handler: { ok: (o: O) => O2, er: (e: E) => E2 }): O2 | E2;
+  abstract await(): Promise<Result<Awaited<O>, Awaited<E>>>;
+}
 
-export const ok = <O>(o: O): Result<O, never> => ({
-  isOk: true,
-  ok: o,
-  unwrap: () => o,
-  match: (handler) => handler.ok(o),
-  handleOk: (handler, onEr) => {
-    if (!onEr) return ok(handler(o));
-    try { return ok(handler(o)); } catch (e2) { return er(onEr(e2)); };
-  },
-  handleEr: () => ok(o),
-});
+class ResultOk<O> extends ResultBase<O, never>{
+  readonly isOk = true;
+  readonly isEr = false;
+  constructor(public ok: O) { super(); };
+  unwrap(): O { return this.ok; };
+  else(): O { return this.ok; };
+  handleOk<O2>(handler: (o: O) => O2): Result<O2, never> { return ok(handler(this.ok)); };
+  handleEr<E2>(): Result<O, E2> { return this; };
+  match<O2, E2>(handler: { ok: (o: O) => O2 }): O2 | E2 { return handler.ok(this.ok); };
+  async await(): Promise<Result<Awaited<O>, Awaited<never>>> { return ok(await this.ok); };
+}
 
-export const er = <E>(e: E): Result<never, E> => ({
-  isOk: false,
-  er: e,
-  unwrap: () => { throw e; },
-  match: (handler) => handler.er(e),
-  handleOk: () => er(e),
-  handleEr: (handler, onEr) => {
-    if (!onEr) return er(handler(e));
-    try { return er(handler(e)); } catch (e2) { return er(onEr(e2)); };
-  },
-});
+class ResultEr<E> extends ResultBase<never, E>{
+  readonly isOk = false;
+  readonly isEr = true;
+  constructor(public er: E) { super(); };
+  unwrap(): never { throw this.er; };
+  else<T>(def: T): T { return def; };
+  handleOk<O2>(): Result<O2, E> { return this; };
+  handleEr<E2>(handler: (e: E) => E2): Result<never, E2> { return er(handler(this.er)); };
+  match<O2, E2>(handler: { er: (e: E) => E2 }): O2 | E2 { return handler.er(this.er); };
+  async await(): Promise<Result<Awaited<never>, Awaited<E>>> { return er(await this.er); };
+}
+
+export type Result<O, E> = ResultOk<O> | ResultEr<E>;
+
+export const ok = <O>(o: O): Result<O, never> => new ResultOk(o);
+export const okVoid = (): Result<void, never> => new ResultOk(undefined);
+export const er = <E>(e: E): Result<never, E> => new ResultEr(e);
