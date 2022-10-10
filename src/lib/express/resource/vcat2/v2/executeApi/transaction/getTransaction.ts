4df8dca0b3613db950a035133bc693869c281b38
charon1212 <charons2turquoise@gmail.com>
2022-10-10 21:48:32 +0900

    fix interface.

--- src/lib/express/resource/vcat2/v2/executeApi/transaction/getTransaction.ts
+++ src/lib/express/resource/vcat2/v2/executeApi/transaction/getTransaction.ts
@@ -1,26 +1,26 @@
 import { Pair } from "../../../../../../../domain/BaseType";
+import { LegalCurrency, VirtualCurrency } from "../../../../../../../domain/Exchange/currency";
 import { CoincheckGetTransactions } from "../../../../../../coincheck/apiTool/CoincheckGetTransactions";
 import { expressResource } from "../../../../../base/resource";
 import { createFailureResponse, createSuccessResponse } from "../../../../../base/response";
 
 type RequestParam = never;
 type RequestBody = never;
-type ResponseData = { transactions: Transaction[], };
-type Transaction = {
+type ResponseData = { transactions: Transaction<Pair>[], };
+type FundLegal<P extends Pair> = { [key in LegalCurrency<P>]: string };
+type FundVirtual<P extends Pair> = { [key in VirtualCurrency<P>]: string };
+type Transaction<P extends Pair> = P extends any ? {
   id: number,
   order_id: number, // 注文のID
   created_at: string, // 取引時間
-  funds: { // 各残高の増減
-    btc: string,
-    jpy: string, // TODO: 貨幣はこれだけじゃない。ETCとかやる場合は追加が必要。
-  },
-  pair: Pair, // 取引ペア
+  funds: FundLegal<P> & FundVirtual<P>, // 各残高の増減
+  pair: P, // 取引ペア
   rate: string, // 約定価格
   fee_currency: string, // 手数料の通貨
   fee: string, // 手数料
   liquidity: 'T' | 'M', // Taker/Maker
   side: 'buy' | 'sell', // 売り/買い
-};
+} : never;
 
 export const getTransaction = () => {
   expressResource<'GET', RequestParam, RequestBody, ResponseData>({
